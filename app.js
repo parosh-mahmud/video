ad3c214aa1ea5a5eb286bfd77a6047028b87307e
Parosh Mahmud <prparosh@gmail.com>
2021-01-12 12:31:37 +0600

    added all files

---
+++ app.js
@@ -0,0 +1,38 @@
+// const btn = document.querySelector(".switch-btn");
+// const video = document.querySelector(".video-container");
+
+// btn.addEventListener("click", function () {
+//   if (!btn.classList.contains("slide")) {
+//     btn.classList.add("slide");
+//     video.pause();
+//   } else {
+//     btn.classList.remove("slide");
+//     video.play();
+//   }
+// });
+
+// // preloader
+// const preloader = document.querySelector(".preloader");
+
+// window.addEventListener("load", function () {
+//   preloader.classList.add("hide-preloader");
+// });
+
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+})
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+  
+})
